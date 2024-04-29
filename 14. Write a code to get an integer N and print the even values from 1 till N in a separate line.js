9c271bc0e3a9646afc095ba9bd93192e5c131636
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2024-04-29 12:57:22 +0530

    Create 14. Write a code to get an integer N and print the even values from 1 till N in a separate line.js
    Input Description:
    A single line contains an integer N.
    
    Output Description:
    Print the even values from 1 to N in a separate line.
    
    Sample Input :
    6
    Sample Output :
    2
    4
    6

---
+++ 14. Write a code to get an integer N and print the even values from 1 till N in a separate line.js
@@ -0,0 +1,26 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+var input = parseInt(userInput[0]);
+ for( var i = 1;i<=input;i++){
+     if (i%2 === 0){
+         console.log(i);
+     }
+ }
+  
+
+  //end-here
+});
